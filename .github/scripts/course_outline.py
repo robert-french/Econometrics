896ed66b351eb940ac91fe901c74ba7af6f4565c
Robert French <robert.timon.french@gmail.com>
2026-09-10 18:45:31 -0700

    release lecture 4

--- .github/scripts/course_outline.py
+++ .github/scripts/course_outline.py
@@ -22,7 +22,7 @@ outline as a placeholder. When the notebook is written, fill in its stem and
 rerun update_sidebars.py.
 """
 
-POSTED_THROUGH = 3
+POSTED_THROUGH = 4
 
 BASE_URL = "https://robert-french.github.io/Econometrics"
 
